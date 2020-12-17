7649e470cb0ec6fda6227c2abb40974a36688599
voldikss <dyzplus@gmail.com>
2020-12-17 18:44:57 +0800

    r

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -1,6 +1,37 @@
-import { promises as fsPromises } from 'fs'
+import fs from 'fs'
 
-export const fsStat = fsPromises.stat
-export const fsWriteFile = fsPromises.writeFile
-export const fsReadFile = filename => fsPromises.readFile(filename, { encoding: 'utf-8' })
-export const fsMkdir = fsPromises.mkdir
+export async function fsStat(filepath: string): Promise<fs.Stats | null> {
+  return new Promise(resolve => {
+    fs.stat(filepath, (err, stats) => {
+      if (err) resolve(null)
+      resolve(stats)
+    })
+  })
+}
+
+export async function fsWriteFile(fullpath: string, content: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fullpath, content, 'utf8', err => {
+      if (err) reject()
+      resolve()
+    })
+  })
+}
+
+export function fsReadFile(fullpath: string, encoding = 'utf8'): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fullpath, encoding, (err, content) => {
+      if (err) reject(err)
+      resolve(content)
+    })
+  })
+}
+
+export function fsMkdir(filepath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(filepath, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+}
